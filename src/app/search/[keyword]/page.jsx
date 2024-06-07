950b87f858d115b684427a00985de3c0942f1437
edangarap <edangarap@gmail.com>
2024-06-07 05:40:20 +0000

    update

--- src/app/search/[keyword]/page.jsx
+++ src/app/search/[keyword]/page.jsx
@@ -1,13 +1,11 @@
 import Header from "@/components/AnimeList/header";
 import AnimeList from "@/components/AnimeList";
+import { getAnime } from "@/app/lib/fetchAPI";
 
 const Page = async ({ params }) => {
    const { keyword } = params;
    const keywordParse = decodeURI(keyword);
-   const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/anime?q=${keywordParse}`
-   );
-   const searchAnime = await response.json();
+   const searchAnime = await getAnime("anime", `q=${keywordParse}`);
 
    return (
       <>
