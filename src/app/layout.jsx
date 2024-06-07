ddfd64c2145d024d7b80439b122e686b7b456278
edangarap <edangarap@gmail.com>
2024-06-07 05:48:47 +0000

    update

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -12,6 +12,13 @@ export const metadata = {
 export default function RootLayout({ children }) {
    return (
       <html lang="en">
+         <head>
+            <script
+               async
+               src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-9962964998808445"
+               crossorigin="anonymous"
+            ></script>
+         </head>
          <body
             className={`${gabarito.className} bg-color-dark`}
             suppressHydrationWarning={true}
@@ -19,6 +26,10 @@ export default function RootLayout({ children }) {
             <Navbar />
             {children}
          </body>
+         <amp-auto-ads
+            type="adsense"
+            data-ad-client="ca-pub-9962964998808445"
+         ></amp-auto-ads>
       </html>
    );
 }
