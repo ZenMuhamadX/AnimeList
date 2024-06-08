b5a78724173df4a8cbae4f06b05d28f3ff880186
muhamadzen669 <muhamadzen669@gmail.com>
2024-06-08 02:53:14 +0000

    update

--- src/app/user/dashboard/page.jsx
+++ src/app/user/dashboard/page.jsx
@@ -4,20 +4,6 @@ import Link from "next/link";
 
 const Page = async () => {
    const user = await authUserSession();
-   if (!user) {
-      return (
-         <div className="min-h-screen max-w-xl mx-auto flex justify-center items-center">
-            <div className=" text-color-primary flex justify-center items-center gap-4 flex-col">
-               <Link
-                  href="/api/auth/signin"
-                  className="text-color-primary bg-color-accent hover:bg-color-primary hover:text-color-accent transition-all text-2xl border rounded-full p-4 "
-               >
-                  Login
-               </Link>
-            </div>
-         </div>
-      );
-   }
    return (
       <div className="text-color-primary">
          <h3>DASHBOARD</h3>
