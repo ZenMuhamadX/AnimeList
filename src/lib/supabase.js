022553c9a18c9dcdf7db720c5108b429f01abdf3
ZenMuhamadX <muhamadzen669@gmail.com>
2024-06-11 21:03:14 +0700

    production

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -1,6 +1,6 @@
 import { createClient } from "@supabase/supabase-js";
 
 export const db = createClient(
-   process.env.NEXT_PUBLIC_SUPABASE_URL,
+   process.env.SUPABASE_URL,
    process.env.SUPABASE_SECRET_KEY
 );
