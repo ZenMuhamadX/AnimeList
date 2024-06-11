dab55828f7a0bb7967051e331198de75d5dd33cc
ZenMuhamadX <muhamadzen669@gmail.com>
2024-06-11 20:40:45 +0700

    update

--- src/app/api/v1/collections/route.js
+++ src/app/api/v1/collections/route.js
@@ -4,7 +4,7 @@ export async function POST(request) {
    const { anime_mal_id, user_email } = await request.json();
    const data = { anime_mal_id, user_email };
 
-   const insert = await db.from("animelist_collection").insert(data);
+   const insert = await db.from("animelist_collection").insert(data).select();
 
    return Response.json({
       response: insert,
