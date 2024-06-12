d827e9e78e5116dcb752009e119bdd4f5c67a395
ZenMuhamadX <muhamadzen669@gmail.com>
2024-06-12 13:24:01 +0700

    fix

--- src/lib/supabase.js
+++ src/lib/supabase.js
@@ -2,5 +2,5 @@ import { createClient } from "@supabase/supabase-js";
 
 export const db = createClient(
    process.env.SUPABASE_URL,
-   process.env.SUPABASE_SECRET_KEY
+   process.env.SUPABASE_SERVICE_ROLE_KEY
 );
