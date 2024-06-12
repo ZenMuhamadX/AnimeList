6b4f96406384e532b5e1b06df7a0e084ad70e977
ZenMuhamadX <muhamadzen669@gmail.com>
2024-06-12 19:44:01 +0700

    fix

--- src/components/AnimeList/CommentInput.jsx
+++ src/components/AnimeList/CommentInput.jsx
@@ -17,11 +17,11 @@ const CommentInput = () => {
          <div className="flex flex-col gap-2">
             <textarea
                onChange={handleComment}
-               className="h-32 w-full text-xl p-4 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
-               placeholder="Tulis komentar Anda di sini..."
+               className="h-32 w-full text-xl p-4 border-2 border-color-accent rounded-lg focus:outline-none focus:border-color-secondary"
+               placeholder="Place your comment..."
             />
             <button
-               className="py-2 px-3 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
+               className="py-2 px-3 bg-color-accent text-color-primary rounded-lg hover:bg-color-primary transition-all duration-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                onClick={handlePosting}
             >
                Posting
