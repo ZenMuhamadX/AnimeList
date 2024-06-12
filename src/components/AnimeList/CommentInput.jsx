323cf7b066d6a35889a41fd2485c145f56ca285a
ZenMuhamadX <muhamadzen669@gmail.com>
2024-06-12 19:39:05 +0700

    sip

--- src/components/AnimeList/CommentInput.jsx
+++ src/components/AnimeList/CommentInput.jsx
@@ -15,17 +15,13 @@ const CommentInput = () => {
    return (
       <>
          <div className="flex flex-col gap-2">
-            <div class="relative w-full min-w-[200px]">
-               <textarea
-                  class="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-purple-500 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
-                  placeholder=" "
-               ></textarea>
-               <label class="before:content[' '] after:content[' '] pointer-events-none absolute left-0 -top-1.5 flex h-full w-full select-none text-[11px] font-normal leading-tight text-blue-gray-400 transition-all before:pointer-events-none before:mt-[6.5px] before:mr-1 before:box-border before:block before:h-1.5 before:w-2.5 before:rounded-tl-md before:border-t before:border-l before:border-blue-gray-200 before:transition-all after:pointer-events-none after:mt-[6.5px] after:ml-1 after:box-border after:block after:h-1.5 after:w-2.5 after:flex-grow after:rounded-tr-md after:border-t after:border-r after:border-blue-gray-200 after:transition-all peer-placeholder-shown:text-sm peer-placeholder-shown:leading-[3.75] peer-placeholder-shown:text-blue-gray-500 peer-placeholder-shown:before:border-transparent peer-placeholder-shown:after:border-transparent peer-focus:text-[11px] peer-focus:leading-tight peer-focus:text-purple-500 peer-focus:before:border-t-2 peer-focus:before:border-l-2 peer-focus:before:border-purple-500 peer-focus:after:border-t-2 peer-focus:after:border-r-2 peer-focus:after:border-purple-500 peer-disabled:text-transparent peer-disabled:before:border-transparent peer-disabled:after:border-transparent peer-disabled:peer-placeholder-shown:text-blue-gray-500">
-                  Textarea Purple
-               </label>
-            </div>
+            <textarea
+               onChange={handleComment}
+               className="h-32 w-full text-xl p-4 border-2 border-gray-300 rounded-lg focus:outline-none focus:border-indigo-500"
+               placeholder="Tulis komentar Anda di sini..."
+            />
             <button
-               className="py-2 px-3 bg-color-accent w-52"
+               className="py-2 px-3 bg-indigo-500 text-white rounded-lg hover:bg-indigo-600 transition-colors duration-300 focus:outline-none focus:ring-2 focus:ring-indigo-500"
                onClick={handlePosting}
             >
                Posting
