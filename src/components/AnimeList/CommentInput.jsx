20b3d9d1a94f5cd3695ef8e61b0e637a5cdcfdb5
ZenMuhamadX <muhamadzen669@gmail.com>
2024-06-12 19:30:54 +0700

    fix

--- src/components/AnimeList/CommentInput.jsx
+++ src/components/AnimeList/CommentInput.jsx
@@ -14,7 +14,7 @@ const CommentInput = () => {
    };
    return (
       <>
-         <div class="relative w-full min-w-[200px]">
+         <div className="flex flex-col gap-2">
             <textarea
                class="peer h-full min-h-[100px] w-full resize-none rounded-[7px] border border-blue-gray-200 border-t-transparent bg-transparent px-3 py-2.5 font-sans text-sm font-normal text-blue-gray-700 outline outline-0 transition-all placeholder-shown:border placeholder-shown:border-blue-gray-200 placeholder-shown:border-t-blue-gray-200 focus:border-2 focus:border-purple-500 focus:border-t-transparent focus:outline-0 disabled:resize-none disabled:border-0 disabled:bg-blue-gray-50"
                placeholder=" "
