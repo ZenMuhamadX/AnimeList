507856ec8067f0e1539e1c17671c67ea03d538af
muhamadzen669 <muhamadzen669@gmail.com>
2024-06-15 12:33:24 +0000

    production

--- src/app/user/dashboard/comment/page.js
+++ src/app/user/dashboard/comment/page.js
@@ -3,18 +3,19 @@ import Header from "@/components/dashboard/header";
 import { authUserSession } from "@/lib/auth-lib";
 import { db } from "@/lib/supabase";
 import Link from "next/link";
+import { redirect } from "next/navigation";
 import React from "react";
 
 const CommentPage = async () => {
    const user = await authUserSession();
-   // if (!user) {
-   //    redirect("/api/auth/signin");
-   // }
+   if (!user) {
+      redirect("/api/auth/signin");
+   }
 
    const comments = await db
       .from("animelist_comment")
       .select("*")
-      .eq("user_email", "muhamadzen669@gmail.com");
+      .eq("user_email", user?.email);
    return (
       <>
          <section className="px-4 mt-4 w-full">
@@ -39,9 +40,6 @@ const CommentPage = async () => {
                            >
                               View comment
                            </Link>
-                           <button className="bg-color-secondary text-color-primary border rounded-md border-color-primary px-4 py-2 transition transform hover:text-color-primary hover:border-color-accent  hover:bg-color-dark hover:scale-105">
-                              Edit Comment
-                           </button>
                            <DeleteComment key={comment.id} id={comment.id} />
                         </div>
                      </div>
